4685a2d2b67e0796e6aeee5f7ad28f6e0f8c0a39
John Carmack <johncarmack@me.com>
2023-01-08 08:36:09 -0800

    initial commit

---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export function Button({ children }) {
+  return (
+    <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+      {children}
+    </button>
+  );
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./Button";
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
