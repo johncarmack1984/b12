d7d20c57eb873a0198fa27a2adb96151568bda1d
John Carmack <johncarmack@me.com>
2023-01-08 21:47:36 -0800

    stopping for the day

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,14 @@
-export function Button({ children }) {
-  return (
-    <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
-      {children}
-    </button>
-  );
+import { createElement, ReactNode } from "react";
+
+export function Button({
+  href,
+  children = "Button",
+  className = "",
+}: {
+  href?: string;
+  children: ReactNode | string;
+  className: string;
+}) {
+  const ButtonEl = href ? "a" : "button";
+  return <button className={className}>{children}</button>;
 }
